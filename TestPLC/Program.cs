73c766b710337c70680aba001ac2f356b5db94c1
Deepak <56668629+deepakmaroo@users.noreply.github.com>
2025-04-20 18:44:26 +0200

    Add files via upload
    Initial release

---
+++ TestPLC/Program.cs
@@ -0,0 +1,34 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using libplctag;
+using System;
+
+Console.WriteLine("PLC Connection test utility!");
+
+TestPLC.TestPLc.initialize_plc();
+try
+{
+    TestPLC.TestPLc.strTag.Initialize();
+    while (true)
+    {
+        try
+        {
+            Console.WriteLine("PLC status " + TestPLC.TestPLc.strTag.GetStatus());
+            TestPLC.TestPLc.strTag.Read();
+            Console.WriteLine("Tag value " + TestPLC.TestPLc.strTag.Value);
+            TestPLC.TestPLc.intTag.Read();
+            Console.WriteLine("Tag value " + TestPLC.TestPLc.intTag.Value);
+            Thread.Sleep(1000);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception caught PLC status " + TestPLC.TestPLc.strTag.GetStatus());
+            Thread.Sleep(2000);
+        }
+    }            
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+
---
+++ TestPLC/TestPLc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libplctag;
+using libplctag.DataTypes;
+
+namespace TestPLC
+{
+    internal class TestPLc
+    {
+        public static Tag<StringPlcMapper, string> strTag;
+        public static Tag<DintPlcMapper, int> intTag;
+        public static String Gateways = "192.168.0.100";
+        public static void initialize_plc()
+        {
+            strTag = new Tag<StringPlcMapper, string>()
+            {
+                Name = "TAG1_NAME",
+                Gateway = Gateways,
+                Path = "1,0",
+                Protocol = Protocol.ab_eip,
+                PlcType = PlcType.ControlLogix,
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+            intTag = new Tag<DintPlcMapper, int>()
+            {
+                Name = "TAG2_NAME",
+                Gateway = Gateways,
+                Path = "1,0",
+                Protocol = Protocol.ab_eip,
+                PlcType = PlcType.ControlLogix,
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+        }
+    }
+
+
+}
